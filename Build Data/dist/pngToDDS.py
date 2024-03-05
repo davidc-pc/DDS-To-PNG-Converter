524db0aebfb72ce0d56793ecd24603140ccea579
davidc-rvw <conti.david9@gmail.com>
2024-03-05 12:41:47 -0500

    Created Executable

---
+++ Build Data/dist/pngToDDS.py
@@ -0,0 +1,34 @@
+from wand.image import Image
+from shutil import move
+import os
+
+def convert_dds_to_png(input_file, output_file):
+    with Image(filename=input_file) as img:
+        img.format = 'png'
+        img.save(filename=output_file)
+
+def move_to_old_files(input_file, old_files_folder):
+    move(input_file, old_files_folder)
+
+def main():
+    input_folder = '.'  # Change this to the folder containing your .dds files
+    output_folder = 'dds_output'  # Output folder for .png files
+    old_files_folder = 'old_files'  # Folder for original .dds files
+    os.makedirs(output_folder, exist_ok=True)
+
+    for filename in os.listdir(input_folder):
+        if filename.endswith(".png"):
+
+            with Image(filename=filename) as img:
+                img.compression = "dxt3"
+                input_file = os.path.join(input_folder, filename)
+                output_file = os.path.join(output_folder, os.path.splitext(filename)[0] + '.dds')
+                img.save(filename=output_file)
+                move_to_old_files(input_file, old_files_folder)
+                print(f"Converted {filename} to DDS")
+
+    # Wait for user input before exiting
+    input("Conversion completed. Press Enter to exit.")
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 To run this program do the following: 
-Put any files you wish to convert inside this folder. Then run either the dds or the png converter by typing the following:
-
-python .\ddsToPNG.py
-python .\pngToDDS.py
+Step 1: Put any files you wish to convert inside this folder
+Step 2: Run either .exe file by double clicking on it
 
 Hit enter and files will be converted automatically and put into their own folders
